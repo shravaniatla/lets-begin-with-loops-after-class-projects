ee51c3398625e1b0fb28d281af90c554a89c6ac2
shravaniatla <shravani.atla13@gmail.com>
2026-03-24 21:24:52 +0530

    Add files via upload

---
+++ check age.py/Check_age.py
@@ -0,0 +1,9 @@
+age = int(input("Enter age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Age is between 10 and 20")
+    else:
+        print("Age is NOT between 10 and 20")
+else:
+    print("Age is NOT between 10 and 20")
